256d1f06cf7f6fb4b0017401fa2250c735872262
Aisha Mirza <84517249+ayshenm@users.noreply.github.com>
2022-10-22 17:59:52 +0400

    education component add

---
+++ src/components/qualification/Qualification.jsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import './qualification.css';
+
+const Qualification = () => {
+    const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
+    return (
+        <section className="qualification section">
+            <h2 className='section_title'>Qualification</h2>
+            <span className="section_subtitle">My personal journey</span>
+
+
+            <div className="qualification_container container">
+
+                <div className="qualification_tab">
+                    <div className={toggleState === 1 ?
+                        "qualification_button qualification_active button--flex" :
+                        "qualification_button button--flex"}
+                        onClick={() => toggleTab(1)}>
+                        <i className=' bx bxs-graduation qualification_icon'>Education</i>
+
+                    </div>
+                    <div className={toggleState === 2 ?
+                        "qualification_button qualification_active button--flex" :
+                        "qualification_button button--flex"}
+                        onClick={() => toggleTab(2)}>
+                        <i className='bx bx-briefcase-alt-2 qualification_icon'>Experience</i>
+                    </div>
+                </div>
+
+                <div className="qualification_sections">
+
+                    <div className={toggleState === 1 ?
+                        "qualification_content qualification_content-active" :
+                        "qualification_content"}>
+
+                        <div className="qualification_data">
+                            <div>
+                                <h3 className='qualification_title'>Computer engineering</h3>
+                                <span className="qualification_subtitle">BDRNK</span>
+                                <div className="qualification_calendar">
+                                    <i className='bx bx-calendar'>2015-2018</i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification_rounded"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification_data">
+                            <div></div>
+                            <div>
+                                <span className="qualification_rounded"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                            <div>
+                                <h3 className='qualification_title'>Computer engineering</h3>
+                                <span className="qualification_subtitle">Sumgait State University</span>
+                                <div className="qualification_calendar">
+                                    <i className='bx bx-calendar'>2021-Present</i>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="qualification_data">
+                            <div>
+                                <h3 className='qualification_title'>Help Desk course</h3>
+                                <span className="qualification_subtitle">Jalal-I</span>
+                                <div className="qualification_calendar">
+                                    <i className='bx bx-calendar'>2019</i>
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification_rounded"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification_data">
+                            <div></div>
+                            <div>
+                                <span className="qualification_rounded"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                            <div>
+                                <h3 className='qualification_title'>Graphic Design course</h3>
+                                <span className="qualification_subtitle">Jalal-I</span>
+                                <div className="qualification_calendar">
+                                    <i className='bx bx-calendar'>2020</i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={toggleState === 2 ?
+                        "qualification_content qualification_content-active" :
+                        "qualification_content"}>
+
+                        <div className="qualification_data">
+                            <div>
+                                <h3 className='qualification_title'>kseroks.az</h3>
+                                <span className="qualification_subtitle">Computer operator</span>
+                                <div className="qualification_calendar">
+                                    <i className='bx bx-calendar'>2019-2020</i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification_rounded"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification_data">
+                            <div></div>
+                            <div>
+                                <span className="qualification_rounded"></span>
+                                <span className="qualification_line"></span>
+                            </div>
+                            <div>
+                                <h3 className='qualification_title'>Tibis</h3>
+                                <span className="qualification_subtitle">Frontend web developer</span>
+                                <div className="qualification_calendar">
+                                    <i className='bx bx-calendar'>2021-Present</i>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Qualification
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -20,17 +20,17 @@ const Services = () => {
                         <i className='bx bx-code services_icon'></i>
                         <h3 className='services_title'>Web Development</h3>
                         <span className="services_button"
-                         onClick={() => toggleTab(1)}>View More
-                        <i className='services_button-icon bx bxs-right-arrow-alt'></i></span>
+                            onClick={() => toggleTab(1)}>View More
+                            <i className='services_button-icon bx bxs-right-arrow-alt'></i></span>
                     </div>
 
-                   
+
 
                     <div className={toggleState === 1 ? "services_modal active_modal" : "services_modal"}>
 
                         <div className="services_modal-content">
-                            <i onClick={() => toggleTab(0)} 
-                            className='bx bxs-message-square-x services_modal-close'>
+                            <i onClick={() => toggleTab(0)}
+                                className='bx bxs-message-square-x services_modal-close'>
                             </i>
                             <h3 className='service_modal-title'>Web page development</h3>
                             <p className="sevices_modal-description">Service with more than 1+ years of experience.
@@ -65,21 +65,21 @@ const Services = () => {
                     <div className='services_multitype'>
                         <i className='bx bxl-sketch services_icon'></i>
                         <h3 className='services_title'>Web Design</h3>
-                        <span className="services_button" 
-                        onClick={() => toggleTab(2)}>View More
-                        <i className='services_button-icon bx bxs-right-arrow-alt'>
-                             </i>
-                            </span>
+                        <span className="services_button"
+                            onClick={() => toggleTab(2)}>View More
+                            <i className='services_button-icon bx bxs-right-arrow-alt'>
+                            </i>
+                        </span>
 
                     </div>
 
-                    
+
 
                     <div className={toggleState === 2 ? "services_modal active_modal" : "services_modal"}>
 
                         <div className="services_modal-content">
                             <i onClick={() => toggleTab(0)}
-                            className='bx bxs-message-square-x services_modal-close'>
+                                className='bx bxs-message-square-x services_modal-close'>
                             </i>
                             <h3 className='service_modal-title'>Web Design</h3>
                             <p className="sevices_modal-description">Service with more than 1+ years of experience.
@@ -114,17 +114,17 @@ const Services = () => {
                         <i className='bx bx-search services_icon'></i>
                         <h3 className='services_title'>Seo</h3>
                         <span className="services_button"
-                         onClick={() => toggleTab(3)}>View More
-                        <i className='services_button-icon bx bxs-right-arrow-alt'>
-                        </i>
+                            onClick={() => toggleTab(3)}>View More
+                            <i className='services_button-icon bx bxs-right-arrow-alt'>
+                            </i>
                         </span>
                     </div>
 
                     <div className={toggleState === 3 ? "services_modal active_modal" : "services_modal"}>
 
                         <div className="services_modal-content">
-                            <i onClick={() => toggleTab(0)} 
-                            className='bx bxs-message-square-x services_modal-close'>
+                            <i onClick={() => toggleTab(0)}
+                                className='bx bxs-message-square-x services_modal-close'>
                             </i>
                             <h3 className='service_modal-title'>Seo</h3>
                             <p className="sevices_modal-description">Service with more than 1+ years of experience.
